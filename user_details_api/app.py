5bb178953f19a99f1560ae1dc3f3999ba0b04943
Sohail Rashid <sohail.ra5@gmail.com>
2019-01-05 16:33:27 -0500

    generate users

--- user_details_api/app.py
+++ user_details_api/app.py
@@ -3,3 +3,30 @@ from flask_restful import Api, Resource, reqparse
 
 app = Flast(__name__)
 api = Api(app)
+
+
+users = [
+	{
+		"name": "Alice",
+		"age": 23,
+		"SIN": "XXX-123-XXX",
+		"occupation": "Web Developer"
+	},
+	{
+		"name": "Kevin",
+		"age": 33,
+		"SIN": "XXX-456-XXX",
+		"occupation": "Civil Engineer"
+	},
+	{
+		"name": "Jenna",
+		"age": 18,
+		"SIN": "XXX-789-XXX",
+		"occupation": "Student"
+	},
+	{
+		"name": "David",
+		"age": 68,
+		"SIN": "XXX-999-XXX",
+		"occupation": "Retired"
+	}

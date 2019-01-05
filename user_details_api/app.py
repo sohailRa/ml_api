3b34dac456dc0555ea1846947e9eb4fe65811a1a
Sohail Rashid <sohail.ra5@gmail.com>
2019-01-05 16:42:57 -0500

    adding CRUD functionality

--- user_details_api/app.py
+++ user_details_api/app.py
@@ -5,7 +5,8 @@ app = Flast(__name__)
 api = Api(app)
 
 
-users = [
+users = 
+[
 	{
 		"name": "Alice",
 		"age": 23,
@@ -30,3 +31,37 @@ users = [
 		"SIN": "XXX-999-XXX",
 		"occupation": "Retired"
 	}
+]
+
+class User(Resource):
+
+	def get(self, name):
+		for user in users:
+			if(name == user['name']):
+				return user, 200
+		return "User not found", 404
+
+	def post(self, name):
+		parser = reqparse.RequestParser()
+		parser.add_argument("age")
+		parser.add_argument("SIN")
+		parser.add_argument("occupation")
+		args = parser.parse_args
+
+		for user in users:
+			if(name == user["name"]):
+				return "User with name {} already exists".format(name), 400
+
+		user = {
+		"name": name,
+		"age": args["age"],
+		"SIN": args["SIN"],
+		"occupation": args["occupation"]
+		}
+		users.append(user)
+		return user, 201
+
+	def put(self, name):
+		pass
+	def delete(self, name):
+		pass

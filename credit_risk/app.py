f22fab08f7ac79db4df658b3fba3547722d5ee0a
Sohail Rashid <sohail.ra5@gmail.com>
2019-01-05 22:53:48 -0500

    init api

--- credit_risk/app.py
+++ credit_risk/app.py
@@ -1,1 +1,19 @@
 from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+app = Flask(__name__)
+api = Api(app)
+
+#Logistic Regression Model object
+lr_clf = LogisticRegression() 
+
+#Loading trained/picked model
+with open("lib/model/model.pkl", 'rb') as f:
+    lr_clf.clf = pickle.load(f)
+
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('request')
--- credit_risk/model.py
+++ credit_risk/model.py
@@ -1,23 +1,6 @@
 import pandas as pd
 import pickle
-from pandas import Series,DataFrame
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import csv
-from sklearn.svm import SVC, LinearSVC
-from sklearn.linear_model import Perceptron
-from sklearn.grid_search import GridSearchCV
-
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
-from sklearn import metrics
-
-
-#Learning curve
-from sklearn.model_selection import learning_curve
-from sklearn.model_selection import ShuffleSplit
-from sklearn.model_selection import validation_curve
 
 #Loading Data
 trainDF = pd.read_csv('lib/data/cs-training.csv', index_col=0)
@@ -27,11 +10,11 @@ trainDF = trainDF.dropna()
 y = trainDF.SeriousDlqin2yrs
 X = trainDF.drop(['SeriousDlqin2yrs'], axis=1)
 
-# regressor = LogisticRegression()
-regressor = LogisticRegression(C= .1, penalty='l1', tol=1e-6)
-regressor.fit(X,y)
+# lr_clf = LogisticRegression()
+lr_clf = LogisticRegression(C= .1, penalty='l1', tol=1e-6)
+lr_clf.fit(X,y)
 #check accuracy
-print ("accuracy is ", regressor.score(X,y)) #mult by 100% to get %accuracy
+print ("accuracy is ", lr_clf.score(X,y)) #mult by 100% to get %accuracy
 
 # Saving model by picking
-pickle.dump(regressor, open('lib/models/model.pkl','wb'), protocol=2)
+pickle.dump(lr_clf, open('lib/models/model.pkl','wb'), protocol=2)

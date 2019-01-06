63da0a42c9ab5bc716bf68ac92240e37b13580bc
Sohail Rashid <sohail.ra5@gmail.com>
2019-01-06 00:42:40 -0500

    debugging predict confidence

--- credit_risk/model.py
+++ credit_risk/model.py
@@ -20,25 +20,41 @@ print ("accuracy is ", lr_clf.score(X,y)) #mult by 100% to get %accuracy
 pickle.dump(lr_clf, open('lib/models/model.pkl','wb'), protocol=2)
 
 
-# print("Testing Predict:\n\n")
-# v = X.iloc[[0]]
-# print(v)
-# print(type(v))
+print("Testing Predict:\n\n")
+aa = X.iloc[[15]]
+# print(v.to_dict(orient='records'))
 
 # v_dict = v.to_dict('records')
-
+# print("-----------------------------------------------\n")
 # req = pd.DataFrame.from_dict(v_dict)
-# print(req)
-# print(type(req))
+# print(req.to_dict(orient='records'))
 
+bb = [{'NumberOfTime60-89DaysPastDueNotWorse': 0.0,
+'NumberOfTime30-59DaysPastDueNotWorse': 2.0,
+'NumberRealEstateLoansOrLines': 6.0,
+'DebtRatio': 0.8029821290000001,
+'NumberOfOpenCreditLinesAndLoans': 13.0,
+'age': 45.0,
+'NumberOfTimes90DaysLate': 0.0,
+'NumberOfDependents': 2.0,
+'MonthlyIncome': 9120.0,
+'RevolvingUtilizationOfUnsecuredLines': 0.7661266090000001}]
 
-# prediction = lr_clf.predict(req)
-# pred_proba = lr_clf.predict_proba(req)
+bb = pd.DataFrame.from_dict(bb)
+data = []
+data.append(bb)
+data.append(aa)
 
-# if prediction == 0:
-#     result = "Default Negative"
-# else:
-#     result = "Default Positive"
-    
-# output = {'prediction': result, 'confidence': pred_proba[0][0]}
-# print(output)
+for dat in data:
+	prediction = lr_clf.predict(dat)
+	pred_proba = lr_clf.predict_proba(dat)
+
+	if prediction == 0:
+	    result = "Default Negative"
+	else:
+	    result = "Default Positive"
+	    
+	output = {'prediction': result, 'confidence': pred_proba[0]}
+	print(output)
+	print("-----------------------------------------------\n\n")
+
